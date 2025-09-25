13cfcbe7cf7d6331d577a852fd12529462def582
Chan <nchan@vpglaserone.ru>
2025-09-25 13:24:24 +0300

    netFramework462

---
+++ libs/libNetFramework462/Features.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libNetFramework462
+{
+    // primary consturctor is feature of LangVersion 12.0
+    public class Features(int i)
+    {
+        /// <summary>
+        ///  need nuget <PackageReference Include="System.ValueTuple" /> 
+        ///  https://www.nuget.org/packages/System.ValueTuple
+        /// </summary>
+        /// <returns></returns>
+        public (int val, int val2) Tuple2()
+        {
+            return (i, 1);
+        }
+    }
+}
